ac0c5e511b3ff07a2e9d04fabcc8ecec31360777
Usha Lama <ushalama200@gmail.com>
2024-07-06 19:17:38 +0100

    method for update is created

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -77,4 +77,40 @@ class StudentController extends Controller
             ], 404);
         }
     }
+
+    public function update(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        } else {
+            $student = Student::find($id);
+            if ($student) {
+                $student->update([
+                    'name' => $request->name,
+                    'course' => $request->course,
+                    'email' => $request->email,
+                    'phone' => $request->phone,
+                ]);
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Updated Successfully"
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => "No Such Student Fount !"
+                ], 500);
+            }
+        }
+    }
 }
