221f882ea713b690e362847b58e189fe7e808d6a
Usha Lama <ushalama200@gmail.com>
2024-07-06 19:13:20 +0100

    method for create student is added

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class StudentController extends Controller
 {
@@ -21,6 +23,42 @@ class StudentController extends Controller
                 'status' => 404,
                 'message' => 'No Records Found'
             ], 404);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|digits:10',
+
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        } else {
+            $students = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+
+            if ($students) {
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Created Successfully"
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something went wrong"
+                ], 500);
+            }
         }
     }
 }
