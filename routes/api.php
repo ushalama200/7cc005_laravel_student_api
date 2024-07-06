275f713ffe6919ed6ad6fa44cdf13d654fd14efd
Usha Lama <ushalama200@gmail.com>
2024-07-06 19:41:07 +0100

    routes created for all controller methods

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\APi\StudentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('students', function () {
+//     return 'this is usha api';
+// });
+
+Route::get('students', [StudentController::class, 'index']);
+Route::post('students', [StudentController::class, 'store']);
+Route::get('students/{id}', [StudentController::class, 'show']);
+Route::get('students/{id}/edit', [StudentController::class, 'edit']);
+Route::put('students/{id}/edit', [StudentController::class, 'update']);
+Route::delete('students/{id}/delete', [StudentController::class, 'destroy']);
