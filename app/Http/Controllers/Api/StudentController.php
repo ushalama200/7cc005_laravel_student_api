f81dda420b263c26e8987965c4d9d3ba7fbf0b3b
Usha Lama <ushalama200@gmail.com>
2024-07-06 19:11:57 +0100

    method for reading all student is created in student controller

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = Student::all();
+        if ($students->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'students' => $students
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Records Found'
+            ], 404);
+        }
+    }
 }
