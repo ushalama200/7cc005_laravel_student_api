02af4e4428e71fa7999c96aeda695718eab88e5b
Usha Lama <ushalama200@gmail.com>
2024-07-06 19:18:40 +0100

    destory method for delete student is added

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -113,4 +113,21 @@ class StudentController extends Controller
             }
         }
     }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => "Student is deleted !"
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => "No Such Student Found !"
+            ], 500);
+        }
+    }
 }
